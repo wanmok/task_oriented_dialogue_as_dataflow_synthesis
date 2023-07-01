bbb4ba8b312acc2e899d9a72b678ddda544c6f2d
WanMok <16273544+wanmok@users.noreply.github.com>
2023-07-01 02:40:41 -0700

    Update setup.py
    Bumped main dependencies to `>=`

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ setup(
     package_data={"dataflow": ["py.typed"]},
     zip_safe=False,
     install_requires=[
-        "jsons==0.10.1",
-        "more-itertools==8.2.0",
-        "sexpdata==0.0.3",
-        "pandas==1.0.0",
-        "spacy==2.2.1",
-        "statsmodels==0.11.1",
-        "cached-property==1.5.1",
+        "jsons>=0.10.1",
+        "more-itertools>=8.2.0",
+        "sexpdata>=0.0.3",
+        "pandas>=1.0.0",
+        "spacy>=2.2.1",
+        "statsmodels>=0.11.1",
+        "cached-property>=1.5.1",
     ],
     extra_requires={
         "OpenNMT-py": ["OpenNMT-py==1.0.0", "pytorch>=1.2.0,<=1.4.0"]
